6c26fd06efde99b53964a16a344c34a27e032b25
MaiBui-code <bui23m@mtholyoke.edu>
2023-11-08 18:13:32 -0500

    add mean bar graph and mean number, change delta stack

--- plotter.py
+++ plotter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class plotter:
@@ -43,9 +44,61 @@ class plotter:
         )
         overlay_df.plot()
         plt.savefig(filename)
-        
-    def overlay_all_jpos(self, id, filename):
-        self.unify_shape()
+
+    def lineGraph2(self, filenames):
+        for i in range(16):
+            jpos = "jpos" + str(i)
+            #plt.clf()
+            df1_column = self.df1[jpos]
+            df2_column = self.df2[jpos]
+            diff = df1_column - df2_column
+            graph = diff.plot(color = "orange")
+            
+            # Add a baseline at 0
+            graph.axhline(0, color='gray', linestyle='--', linewidth=1)
+            plt.savefig(filenames[i])
+
+    def mean_bar_graph(self, filename):
+
+        #create mean, jpos column and bar_color for positive mean = green, negative mean = orange
+        mean_column = []
+        jpos_column = []
+        bar_color = []
+        for i in range(16):
+            jpos = "jpos" + str(i)
+            plt.clf()
+            # count mean
+            df1_column = self.df1[jpos]
+            df2_column = self.df2[jpos]
+            diff = df1_column - df2_column
+            mean = diff.mean()
+            if mean < 0:
+                bar_color.append("orange")
+            elif mean > 0:
+                bar_color.append("g")
+            else:
+                bar_color.append("b")
+            mean_column.append(mean)
+            jpos_column.append(jpos)
+
+        #create pandas dataframe with above columns
+        mean_bar = pd.DataFrame({"Mean Differences": mean_column, "Joint Position": jpos_column})
+        # create bar plots        
+        graph = mean_bar.plot(kind = "bar", x = "Joint Position", y = "Mean Differences", legend=False, color=bar_color)
+        # Add a baseline at 0
+        graph.axhline(0, color='gray', linestyle='--', linewidth=1)
+        # Add labels and title
+        plt.xlabel("Mean Differences")
+        plt.ylabel("Joint Position")
+        plt.title("Mean Differences of Joint Positions")
+
+        #beside generating graph, also count the mean number of all means to represent the overal performance
+        mean_error = sum(abs(mean) for mean in mean_column)/14
+        #add overal mean in the right corner
+        graph.text(0.95, 0.95, f"Mean Error: {mean_error:.2f}", transform=graph.transAxes, fontsize=12,
+            verticalalignment='top', horizontalalignment='right', bbox=dict(boxstyle='round,pad=0.5', edgecolor='gray', facecolor='white'))
+        plt.savefig(filename)
+        return
 
 def create_path(i):
     return "2023-10-28 17_54_29/test_mai_trial" + str(i) + ".csv"
@@ -58,9 +111,10 @@ def main():
     check = plotter(pd.read_csv(csv_path[0]), pd.read_csv(csv_path[3]))
     # save fig
     graph_path = "data_analysis/"
-    check.plot_one_jpos(4, graph_path + "one_pos.png")
+    check.plot_one_jpos(2, graph_path + "one_pos.png")
     check.plot_all_jpos(graph_path + "all_jpos.png")
-    check.overlay_one_jpos(5, graph_path + "overlay.png")
-
+    check.overlay_one_jpos(2, graph_path + "overlay.png")
+    check.mean_bar_graph(graph_path + "mean_bar_graph.png")
+    
 if __name__ == "__main__":
     main()
